fbea53d1a7e53f8bad7eb9195e9d61ea23f9eab1
Jkx777 <rainstorm886@gmail.com>
2026-09-19 19:46:13 +0000

    c4

--- uber_pickups.py
+++ uber_pickups.py
@@ -5,7 +5,20 @@ import numpy as np
 st.title('Uber pickups in NYC')
 st.write('Hello world!')
 
-if st.button('Say hello'):
-     st.write('Why hello there')
-else:
-     st.write('Goodbye')
+# if st.button('Say hello'):
+#      st.write('Why hello there')
+# else:
+#      st.write('Goodbye')
+
+
+import streamlit as st
+
+n1, n2 = st.number_input("Insert a number"), st.number_input("Insert a number")
+if st.button('minus'):
+    st.write(n1-n2)
+else if st.button('plus'):
+    st.write(n1 + n2)
+else if st.button('multiply'):
+    st.write(n1*n2)
+else if st.button('divie'):
+    st.write(n1/n2)
