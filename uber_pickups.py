2649c3ef8874427a7ccdb580079615aa25a70efe
Jkx777 <rainstorm886@gmail.com>
2026-09-24 22:37:39 +0000

    c9

--- uber_pickups.py
+++ uber_pickups.py
@@ -8,28 +8,30 @@ st.title('Uber pickups in NYC')
 st.write('Hello world!')
 
 #====================================================================
-option = st.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"),
-)
-
-st.write("You selected:", option)
-#====================================================================
 
 n1= st.number_input("Insert a number")
 
 n2 = st.number_input(
     "Insert a number", value=None, placeholder="Type a number..."
+
+#selectionbox-----------------------------------
+)
+option = st.selectbox(
+    "How would you like to be contacted?",
+    ("Addition", "Subtraction", "Division", "Multiplication"),
 )
 
-if st.button('minus'):
-    st.write(n1-n2)
+st.write("You selected:", option)
 
-if st.button('plus'):
-    st.write(n1 + n2)
+#Buttons------------------------------------------
+# if st.button('minus'):
+#     st.write(n1-n2)
 
-if st.button('multiply'):
-    st.write(n1*n2)
+# if st.button('plus'):
+#     st.write(n1 + n2)
 
-if st.button('divied'):
-    st.write(n1/n2)
+# if st.button('multiply'):
+#     st.write(n1*n2)
+
+# if st.button('divied'):
+#     st.write(n1/n2)
