20f99c068fe4c9ffdb00235d3286b75e242b7bab
Jkx777 <rainstorm886@gmail.com>
2026-09-26 19:00:23 +0000

    c3

--- uber_pickups.py
+++ uber_pickups.py
@@ -21,7 +21,35 @@ if st.button("Search"):
     st.write("Wieght: ", res["weight"])
     st.write("Height: ", res["height"])
     st.write("Ability: ", res["abilities"][0]["ability"]["name"])
+
     if len(res["types"]) == 1:
         st.write("Types: ", res["types"][0]["type"]["name"])
     else:
         st.write("Types: ", res["types"][0]["type"]["name"], " & ", res["types"][1]["type"]["name"])
+
+    //Base states
+    hp, atk, defe, Satk, Sdefe, sp = st.columns(6)
+
+    with hp:
+        st.write("Hp")
+        st.write(res["states"][0]["base_stat"])
+
+    with atk:
+        st.write("Attack")
+        st.write(res["states"][1]["base_stat"])
+
+    with defe:
+        st.write("Defense")
+        st.write(res["states"][2]["base_stat"])
+
+    with Satk:
+        st.write("Special attack")
+        st.write(res["states"][3]["base_stat"])
+
+    with Sdefe:
+        st.write("Special defense")
+        st.write(res["states"][4]["base_stat"])
+
+    with sp:
+        st.write("Speed")
+        st.write(res["states"][5]["base_stat"])
